2a8c544de7ee9353c86a9b2c4c777ecdc0da0c34
Zee <zee@zincma.de>
2020-01-06 08:37:44 -0800

    Make index.js more parseable at a glance
    The longer a file is, the more difficult it is to determine what that
    particular file does at a glance.
    
    Further, I have a slight preference that any given file either defines a module
    for use in other modules, or combines modules to perform a particular
    function.
    
    By splitting out build and publish into discrete modules, I encapsulate
    responsibility for these actions with responsibility for interacting
    with the external modules they use.

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const ncp = require('ncp').ncp;
-const ghpages = require('gh-pages');
+const build = require('./lib/build');
+const publish = require('./lib/publish');
 
-ncp.limit = 16;
-
-ncp('src', 'dist', function (err) {
- if (err) {
-   return console.error(err);
- }
- console.log('copy done!');
- ghpages.publish('dist', () => console.log('Publish complete'));
-});
-
-
+build().then(publish)
