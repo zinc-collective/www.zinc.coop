c09985216e38814b4c8582b661f07a9ae9edb3d4
Ana Ulin <ana@ulin.org>
2020-11-28 13:27:31 -0800

    Apply colors, menu and footer treatment from mocks.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,14 @@ module.exports = {
   plugins: [
     require('@tailwindcss/ui')
   ],
+  theme: {
+    extend: {
+      colors: {
+        'zinc-purple': '#332658',
+        'zinc-dark-purple': '#1F1735',
+        'zinc-hot-pink': '#F43EF3',
+        'zinc-pastel-pink': '#FE9BFF',
+      }
+    }
+  }
 }
