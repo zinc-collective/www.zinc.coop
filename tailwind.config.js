1c15cc73f0fc8b793d1d8a6b233266d70ba85d46
Kelly Hong <k_ahong@yahoo.com>
2023-06-15 21:04:10 -0700

    WIP - add more WCAG compliance colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'zinc-dark-purple': '#1F1735',
         'zinc-pastel-pink': '#FE9BFF',
         'zinc-pastel-blue': '#9BE4FF',
+        'zinc-pastel-yellow': '#FFE99B',
+        'zinc-pastel-green': '#9BFFA3',
+        'zinc-pastel-purple': '#A79BFF',
+        'zinc-pastel-red': '#FF9B9B',
       }
     }
   }
