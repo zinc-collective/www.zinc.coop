9ac396b6053ddf5d5f95bba245b7e3b317a3bde8
ci-build <ci-build@zinc.coop>
2025-06-11 22:16:55 +0000

    Deploy to GitHub pages [ci skip]

--- src/convene/convene.11tydata.js
+++
@@ -1,5 +0,0 @@
-const data = {
-  stripeKey: process.env.STRIPE_PUBLISHABLE_API_KEY || "Missing STRIPE_PUBLISHABLE_API_KEY",
-  priceId: process.env.CONVENE_PRICE_ID || "Missing CONVENE_PRICE_ID"
-}
-module.exports = data
